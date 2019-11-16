e1e92e53fe315977485a927a82ce64c28400243e
ssk <ss.kovalevskiy@yandex.ru>
2019-11-16 18:23:04 +0300

    Добавил решение задачи 15

--- src/main/java/Task15.java
+++ src/main/java/Task15.java
@@ -1,3 +1,8 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  * Написать программу сортировки по возрастанию заданного пользователем массива чисел.
  * Использовать пузырьковый метод сортировки.
@@ -5,5 +10,35 @@
 public class Task15 {
     public static void main(String[] args) {
 
+        System.out.println("Введите числа через пробел");
+        Scanner in = new Scanner(System.in);
+
+        String array[] = in.nextLine().split(" ");
+
+        int numbers[] = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            numbers[i] = Integer.parseInt(array[i]);
+        }
+
+        numbers = insertIntoSort(numbers);
+        System.out.println("Массив чисел после сортировки: " + Arrays.toString(numbers));
+    }
+
+    public static int[] insertIntoSort(int[] arr) {
+        int temp, j;
+        for(int i = 0; i < arr.length - 1; i++){
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                j = i;
+                while (j > 0 && temp < arr[j - 1]) {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[j] = temp;
+            }
+        }
+        return arr;
     }
 }
